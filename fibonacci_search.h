bc1fd59b2403ba5071edbb26190ae8994e206ad7
CodeBreeze1024 <1533053094@qq.com>
2021-03-26 15:27:15 +0800

    2022-3-16 Create a Fibonacci Search Class.

---
+++ fibonacci_search.h
@@ -0,0 +1,15 @@
+/* Fibonacci Search
+ * 2021-3-36 
+ * Jia Qiang
+ * Description:This is a test class for Fibonacci Search.
+ * Fibonacci Array is defined as follow:
+ *              0            when n==0;
+ * F(n)=        1            when n==1;
+ *        F(n=1)+F(n-2)      else               
+ * */
+#include<iostream>
+
+class FibSearch()
+{
+
+};
